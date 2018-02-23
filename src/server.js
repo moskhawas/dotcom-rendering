ee3f9576433c13532bdd750cf7c9496dc34d6512
Simon Adcock <simon.adcock@guardian.co.uk>
2018-02-23 11:58:16 +0000

    server javascript

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 // @flow
 
+import fs from 'fs';
+import path from 'path';
+import pify from 'pify';
 import { renderToString } from 'react-dom/server';
 import Styletron from 'styletron-server';
 import { StyletronProvider } from 'styletron-react';
 import doc from 'lib/__html';
+
+const readFile = pify(fs.readFile);
 
 const renderPage = async function renderPage(pageName) {
     const page = await import(`./pages/${pageName}`);
@@ -23,7 +28,13 @@ const renderPage = async function renderPage(pageName) {
 };
 
 export default async (req: {}): string => {
-    if (req.url.includes('/pages/')) { // TODO: regex plz
+    if (req.url.includes('/pages/')) {
         return renderPage(req.url.split('/pages/')[1]);
+    }
+    if (req.url.includes('/assets/javascript/')) {
+        return readFile(
+            path.join('dist', req.url.split('/assets/javascript/')[1]),
+            'utf8',
+        );
     }
 };
