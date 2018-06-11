fe723fa5de86f0f3894a27cf1c94a4a74c86bf62
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-06-11 11:03:46 +0100

    Add ProgressBar component

---
+++ sites/dotcom/components/Audio/ProgressBar.js
@@ -0,0 +1,86 @@
+import { Component, styled } from '@guardian/guui';
+
+import AuralAid from './AuralAid';
+import Slider from './Slider';
+
+const Progress = styled('div')(
+  ({ backgroundColor }) => ({
+    alignItems: 'stretch',
+    backgroundColor,
+    display: 'flex',
+    height: '6px',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    position: 'relative',
+    width: '100%'
+  })
+);
+
+const Track = styled('div')(
+  ({ backgroundColor, width }) => ({
+    backgroundColor,
+    width
+  })
+);
+
+const pct = n => `${n}%`;
+
+export default class ProgressBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tracking: false,
+      value: props.value,
+      position: 0,
+      width: 0
+    };
+  }
+  
+  componentDidMount() {
+    const { width, left } = this.element.getBoundingClientRect();
+    const position = this.state.value * width / 100;
+    this.setState({ width, left, position });
+  }
+
+  getElement = el => {
+    this.element = el;
+  }
+
+  start = e => {
+    const position = e.clientX - this.state.left;
+    const value = position * 100 / this.state.width;
+    this.setState({ tracking: true, position, value });
+  }
+
+  stop = () => {
+    this.setState({ tracking: false });
+    this.onChange(this.state.value);
+  }
+
+  update = e => {
+    const position = e.clientX - this.state.left;
+    const value = position * 100 / this.state.width;
+    this.setState({ value, position });
+  }
+
+  render({ formattedValue, progressColour, trackColour }, { value, tracking, position, width }) {
+    return (
+      <Progress 
+        innerRef={this.getElement} 
+        backgroundColor={progressColour}
+        onMouseDown={this.start}
+        onMouseUp={tracking ? this.stop : false}
+        onMouseMove={tracking ? this.update : false}
+        role="progressbar" 
+        aria-valuenow={value} 
+        aria-valuemin="0" 
+        aria-valuemax="100"
+        >
+        <Track backgroundColor={trackColour} width={pct(value)} />
+        <AuralAid text={formattedValue} />
+        <Slider min={0} max={width} value={position} />
+      </Progress>
+    )
+  }
+}
