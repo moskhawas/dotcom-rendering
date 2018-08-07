e13b35616eb428b2fabb90b2d172c4ed90a840d0
Simon Adcock <simon.adcock@guardian.co.uk>
2018-08-07 18:39:07 +0100

    refactor dropdown with styled

--- packages/guui/dropdown/index.js
+++ packages/guui/dropdown/index.js
@@ -7,6 +7,7 @@ import { styled, Component } from '@guardian/guui';
 import palette from '@guardian/pasteup/palette';
 import { textSans } from '@guardian/pasteup/fonts';
 import { screenReaderOnly } from '@guardian/pasteup/mixins';
+import { withProps } from 'recompose';
 
 export type Link = {
     url: string,
@@ -20,131 +21,125 @@ type Props = {
     links: Array<Link>,
 };
 
-const Div = styled('div')({
-    'button, label': {
-        display: 'block',
-        cursor: 'pointer',
-        background: 'none',
-        border: 'none',
-        lineHeight: 1.2,
+const EditionButton = styled('button')`
+    display: block;
+    cursor: pointer;
+    background: none;
+    border: none;
+    line-height: 1.2rem;
+    font-size: 14px;
+    font-family: ${textSans};
+    color: ${palette.neutral[1]};
+    transition: color 80ms ease-out;
+    padding: 6px 10px;
+    margin: 1px 0 0;
+    text-decoration: none;
 
-        fontSize: 14,
-        fontFamily: textSans,
-        color: palette.neutral['1'],
-        transition: 'color 80ms ease-out',
-        padding: '6px 10px',
-        margin: '1px 0 0',
-        textDecoration: 'none',
-        ':hover': {
-            textDecoration: 'underline',
-        },
-        ':focus': {
-            textDecoration: 'underline',
-        },
+    &:hover {
+        text-decoration: underline;
+    }
 
-        ':after': {
-            content: '""',
-            display: 'inline-block',
-            width: '4px',
-            height: '4px',
-            transform: 'translateY(-2px) rotate(45deg)',
-            border: '1px solid currentColor',
-            borderLeft: 'transparent',
-            borderTop: 'transparent',
-            marginLeft: '6px',
-            verticalAlign: 'middle',
-            transition: 'transform 250ms ease-out',
-        },
-    },
+    &:focus {
+        text-decoration: underline;
+    }
 
-    input: {
-        ...screenReaderOnly,
-        ':checked + ul': {
-            display: 'block',
-        },
-    },
+    &:after {
+        content: '';
+        display: inline-block;
+        width: 4px;
+        height: 4px;
+        border: 1px solid currentColor;
+        border-left: transparent;
+        border-top: transparent;
+        margin-left: 6px;
+        vertical-align: middle;
+        transition: transform 250ms ease-out;
+        transform: ${({ expanded }) =>
+            expanded ? 'translateY(1px) rotate(-135deg)' : 'translateY(-2px) rotate(45deg)'
+        };
+    }
+`;
 
-    'button.expanded:after, label.expanded:after': {
-        transform: 'translateY(1px) rotate(-135deg)',
-    },
+const EditionLabel = EditionButton.withComponent('label');
+const EditionCheckbox = withProps({
+    type: 'checkbox',
+})(styled('input')`
+    ${screenReaderOnly};
+    &:checked + ul {
+        display: 'block';
+    }
+`);
 
-    ul: {
-        zIndex: 1072,
-        listStyle: 'none',
-        backgroundColor: 'white',
-        padding: '6px 0',
-        boxShadow: '0 0 0 1px rgba(0,0,0,0.1)',
-        borderRadius: '3px',
-        position: 'absolute',
-        right: '0',
-        width: '200px',
-        display: 'none',
-    },
+const EditionList = styled('ul')`
+    z-index: 1072;
+    list-style: none;
+    background-color: white;
+    padding: 6px 0;
+    box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.1);
+    border-radius: 3px;
+    position: absolute;
+    right: 0;
+    width: 200px;
+    display: ${({ expanded }) => (expanded ? 'block' : 'none')};
+`;
 
-    'ul.expanded': {
-        display: 'block',
-    },
+const isActive = `
+    font-weight: bold;
 
-    'li a': {
-        display: 'block',
-        padding: '10px 18px 15px 30px',
-    },
+    &:after {
+        content: '';
+        border: 2px solid #c70000;
+        border-top: 0;
+        border-right: 0;
+        position: absolute;
+        top: 14px;
+        left: 10px;
+        width: 10px;
+        height: 4px;
+        transform: rotate(-45deg);
+    }
+`;
+const EditionLink = styled('a')`
+    display: block;
+    padding: 10px 18px 15px 30px;
+    font-size: 15px;
+    font-family: ${textSans};
+    color: ${palette.neutral[1]};
+    line-height: 1.2rem;
+    position: relative;
+    transition: color 80ms ease-out;
+    margin: -1px 0 0 0;
+    text-decoration: none;
 
-    a: {
-        fontSize: '15px',
-        fontFamily: textSans,
-        color: palette.neutral['1'],
-        lineHeight: 1.2,
-        position: 'relative',
-        transition: 'color 80ms ease-out',
-        padding: '6px 10px',
-        margin: '-1px 0 0 0',
-        textDecoration: 'none',
-        ':hover': {
-            textDecoration: 'underline',
-        },
-        ':focus': {
-            textDecoration: 'underline',
-        },
+    :hover {
+        background-color: #ededed;
+        text-decoration: underline;
+    }
 
-        ':before': {
-            content: '""',
-            borderTop: '1px solid #ededed',
-            display: 'block',
-            position: 'absolute',
-            top: 0,
-            left: '30px',
-            right: 0,
-        },
-    },
+    :focus {
+        text-decoration: underline;
+    }
 
-    // no top border for first item
-    'li:first-child a:before': {
-        content: 'none',
-    },
+    :before {
+        content: '';
+        border-top: 1px solid #ededed;
+        display: block;
+        position: absolute;
+        top: 0;
+        left: 30px;
+        right: 0;
+    }
 
-    'a:hover': {
-        backgroundColor: '#ededed',
-        textDecoration: 'none',
-    },
+    ${({ active }) => (active ? isActive : '')};
+`;
 
-    'a.active': {
-        fontWeight: 'bold',
+const EditionItem = styled('li')`
+    &:first-child ${EditionLink}:before {
+        content: none;
+    }
+`;
 
-        ':after': {
-            content: '""',
-            border: '2px solid #c70000',
-            borderTop: 0,
-            borderRight: 0,
-            position: 'absolute',
-            top: '14px',
-            left: '10px',
-            width: '10px',
-            height: '4px',
-            transform: 'rotate(-45deg)',
-        },
-    },
-});
+/* no top border for first item */
 
 export default class Dropdown extends Component<
     Props,
@@ -192,19 +187,18 @@ export default class Dropdown extends Component<
         const checkboxID = `checkbox-id-${this.props.id}`;
 
         return (
-            <Div>
+            <div>
                 {this.state.noJS ? (
                     [
-                        <label
+                        <EditionLabel
                             htmlFor={checkboxID}
-                            className={this.state.isExpanded ? 'expanded' : ''}
                             aria-controls={dropdownID}
                             aria-expanded={
                                 this.state.isExpanded ? 'true' : 'false'
                             }
+                            expanded={this.state.isExpanded}
                         >
-                            <input
-                                type="checkbox"
+                            <EditionCheckbox
                                 id={checkboxID}
                                 aria-controls={dropdownID}
                                 aria-checked="false"
@@ -213,35 +207,29 @@ export default class Dropdown extends Component<
                                 role="menuitemcheckbox"
                             />
                             {label}
-                        </label>,
+                        </EditionLabel>,
                     ]
                 ) : (
-                    <button
+                    <EditionButton
                         onClick={this.toggle_}
-                        className={this.state.isExpanded ? 'expanded' : ''}
                         aria-controls={dropdownID}
                         aria-expanded={this.state.isExpanded ? 'true' : 'false'}
+                        expanded={this.state.isExpanded}
                     >
                         {label}
-                    </button>
+                    </EditionButton>
                 )}
 
-                <ul
-                    id={dropdownID}
-                    className={this.state.isExpanded ? 'expanded' : ''}
-                >
+                <EditionList id={dropdownID} expanded={this.state.isExpanded}>
                     {links.map(link => (
-                        <li key={link.title}>
-                            <a
-                                href={link.url}
-                                className={link.isActive ? 'active' : ''}
-                            >
+                        <EditionItem key={link.title}>
+                            <EditionLink href={link.url} active={link.isActive}>
                                 {link.title}
-                            </a>
-                        </li>
+                            </EditionLink>
+                        </EditionItem>
                     ))}
-                </ul>
-            </Div>
+                </EditionList>
+            </div>
         );
     }
 }
